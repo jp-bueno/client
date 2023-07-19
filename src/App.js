01cab382d1d6f2a9c53912b6219cc9e7fd92fa66
bueno <joaopedrobueno2910@gmail.com>
2023-07-19 11:17:37 -0300

    feat: Noções Básicas de React
    feat: Noções Básicas de React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Header from './Header'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  //Array [value, changeValueFunction]
+
+  function increment() {
+    setCounter(counter + 1)
+  }
+
+  return(
+    <div>
+    	<Header>
+        Counter:{counter}
+      </Header>
+      <button onClick={increment}>Add</button>
     </div>
   );
 }
-
-export default App;
