afdf02ca76fbb03efad713d3eedd7055a894d3e6
bueno <joaopedrobueno2910@gmail.com>
2023-07-19 14:33:22 -0300

    finalizando primeira etapa front
    finalizando primeira etapa front

--- src/pages/Book/index.js
+++ src/pages/Book/index.js
@@ -5,13 +5,13 @@ import { FiPower, FiEdit, FiTrash2 } from "react-icons/fi";
 import './style.css'
 import logoImg from '../../assets/logo.svg'
 
-export default function Book() {
+export default function Books() {
     return (
         <div className="book-container">
             <header>
                 <img src={logoImg} alt="Erudio"/>
                 <span>Welcome, <strong>João</strong>!</span>
-                <Link className="button" to="book/new">Add new book</Link>
+                <Link className="button" to="/book/new">Add new book</Link>
                 <button type="button">
                     <FiPower size={18} color="#251fc5"/>
                 </button>
